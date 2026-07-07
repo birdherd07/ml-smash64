c32bce927fa640bbd1d56587bac4f78cc061c22f
Rachel <greendiamonddove@hotmail.com>
2026-07-07 01:48:25 -0500

    P1 and P2 damage and KO count

---
+++ ssb_playerinfo.lua
@@ -0,0 +1,50 @@
+-- This template lives at `.../Lua/.template.lua`.
+
+-- DWORD unsigned Big Endian
+local p1_dmg_loc = 0x0A4D74
+local p2_dmg_loc = 0x0A4DE8
+local p1_death_loc = 0x0A4D38
+local p2_death_loc = 0x0A4D3C
+
+local txt_color = 0xFF000000
+local txt_buf = 10
+
+local players = {
+	{p = 1, dmg = 0, deaths = 0},
+	{p = 2, dmg = 0, deaths = 0}
+}
+
+local function update_dmg()
+	players[1].dmg = mainmemory.read_u32_be(p1_dmg_loc)
+	players[2].dmg = mainmemory.read_u32_be(p2_dmg_loc)
+end
+
+local function update_deaths()
+	players[1].deaths = mainmemory.read_u32_be(p1_death_loc)
+	players[2].deaths = mainmemory.read_u32_be(p2_death_loc)
+end
+
+local function draw_vals()
+	local i = 1
+	for index, player_vals in ipairs(players) do
+		-- print player name first (unordered table)
+		gui.drawText(txt_buf, txt_buf * i, "P"..player_vals.p, txt_color, 9)
+		i = i + 1
+		-- print rest
+		for k, v in pairs(player_vals) do
+			if k ~= 'p' then
+				gui.drawText(txt_buf, txt_buf * i, k..": "..v, txt_color, 9)
+				i = i + 1
+			end
+		end
+	end
+end
+
+while true do
+	update_dmg()
+	update_deaths()
+
+	draw_vals()
+
+	emu.frameadvance();
+end
