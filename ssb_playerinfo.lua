dfd239d30cb2917c3fc06cead3907c9866a2d5e3
Rachel <greendiamonddove@hotmail.com>
2026-07-07 03:08:23 -0500

    Added x position, y wip

--- ssb_playerinfo.lua
+++ ssb_playerinfo.lua
@@ -6,13 +6,25 @@ local p2_dmg_loc = 0x0A4DE8
 local p1_death_loc = 0x0A4D38
 local p2_death_loc = 0x0A4D3C
 
+-- floats
+local p2_x_loc = 0x2F8BA4
+local p1_x_loc = 0x2F48B4
+
 local txt_color = 0xFF000000
 local txt_buf = 10
 
 local players = {
-	{p = 1, dmg = 0, deaths = 0},
-	{p = 2, dmg = 0, deaths = 0}
+	{p = 1, dmg = 0, deaths = 0, x = 0, y = 0},
+	{p = 2, dmg = 0, deaths = 0, x = 0, y = 0}
 }
+
+local function update_x()
+	-- rounding to 4 places
+	local unrounded_x1 = mainmemory.readfloat(p1_x_loc, true);
+	players[1].x = math.floor(unrounded_x1 * 10000 + 0.5) / 10000
+	local unrounded_x2 = mainmemory.readfloat(p2_x_loc, true);
+	players[2].x = math.floor(unrounded_x2 * 10000 + 0.5) / 10000
+end
 
 local function update_dmg()
 	players[1].dmg = mainmemory.read_u32_be(p1_dmg_loc)
@@ -43,6 +55,7 @@ end
 while true do
 	update_dmg()
 	update_deaths()
+	update_x()
 
 	draw_vals()
 
